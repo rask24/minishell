19b1481541d45b8b5de037af8b84f84c40fe55f5
yliu <yliu@XLIJFELKNIX.local>
2024-08-22 22:02:42 +0900

    [fix] string to cstring

--- tests/unit/test_token.cpp
+++ tests/unit/test_token.cpp
@@ -1,30 +1,28 @@
-#include <string>
+#include <cstring>
 
 #include "gtest/gtest.h"
 
-extern "C"
-{
+extern "C" {
 #include "../include/token.h"
 }
 
-TEST(CheckToken, Check_token_list)
-{
-	t_token_list *list = construct_token_list();
+TEST(CheckToken, Check_token_list) {
+  t_token_list *list = construct_token_list();
 
-	// ls && cat
-	t_token *token = construct_token(strdup("ls"), TOKEN_WORD);
-	t_token *token1 = construct_token(strdup("&&"), TOKEN_AND_IF);
-	t_token *token2 = construct_token(strdup("cat"), TOKEN_WORD);
+  // ls && cat
+  t_token *token = construct_token(strdup("ls"), TOKEN_WORD);
+  t_token *token1 = construct_token(strdup("&&"), TOKEN_AND_IF);
+  t_token *token2 = construct_token(strdup("cat"), TOKEN_WORD);
 
-	push_back_token(&list, token);
-	push_back_token(&list, token1);
-	push_back_token(&list, token2);
+  push_back_token(&list, token);
+  push_back_token(&list, token1);
+  push_back_token(&list, token2);
 
-	t_token *current = (t_token *)list->content;
-	EXPECT_STREQ(current->value, "ls");
-	EXPECT_EQ(current->type, TOKEN_WORD);
+  t_token *current = (t_token *)list->content;
+  EXPECT_STREQ(current->value, "ls");
+  EXPECT_EQ(current->type, TOKEN_WORD);
 
-	EXPECT_EQ(ft_lstsize(list), 3);
+  EXPECT_EQ(ft_lstsize(list), 3);
 
-	destroy_token_list(list);
+  destroy_token_list(list);
 }
