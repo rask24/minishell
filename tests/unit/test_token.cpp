ae81f359ca68bd0f11b5ae38f1c0866d351263ef
rask24 <rask0x18@gmail.com>
2024-08-28 23:46:57 +0900

    Fix test_token

--- tests/unit/test_token.cpp
+++ tests/unit/test_token.cpp
@@ -6,17 +6,16 @@ extern "C" {
 #include "token.h"
 }
 
-TEST(CheckToken, CheckOneToken) {
+TEST(construct_token, CheckOneToken) {
   t_token_list *token = construct_token(TOKEN_WORD, strdup("ls"));
 
-  t_token *current = (t_token *)token->content;
-  EXPECT_STREQ(current->value, "ls");
-  EXPECT_EQ(current->type, TOKEN_WORD);
+  EXPECT_EQ(get_token_type(token), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(token), "ls");
 
   destroy_token(token);
 }
 
-TEST(CheckToken, CheckTokens) {
+TEST(construct_token, CheckMultipleTokens) {
   t_token_list *list = NULL;
 
   // ls && cat
@@ -28,11 +27,17 @@ TEST(CheckToken, CheckTokens) {
   ft_lstadd_back(&list, token1);
   ft_lstadd_back(&list, token2);
 
-  t_token *current = (t_token *)list->content;
-  EXPECT_STREQ(current->value, "ls");
-  EXPECT_EQ(current->type, TOKEN_WORD);
+  // ls
+  EXPECT_EQ(get_token_type(list), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(list), "ls");
 
-  EXPECT_EQ(ft_lstsize(list), 3);
+  // &&
+  EXPECT_EQ(get_token_type(list->next), TOKEN_AND_IF);
+  EXPECT_STREQ(get_token_value(list->next), "&&");
+
+  // cat
+  EXPECT_EQ(get_token_type(list->next->next), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(list->next->next), "ca");
 
   destroy_token_list(list);
 }
