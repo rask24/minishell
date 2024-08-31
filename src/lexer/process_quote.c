8941e220232a8c3a659e5db142117f55f640d75e
yliu <yliu@XLIJFELKNIX.local>
2024-08-31 14:10:07 +0900

    Add exit to error msg

--- src/lexer/process_quote.c
+++ src/lexer/process_quote.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 22:45:32 by yliu              #+#    #+#             */
-/*   Updated: 2024/08/31 11:41:18 by yliu             ###   ########.fr       */
+/*   Updated: 2024/08/31 14:06:54 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	process_quote(t_lexer *lexer, char left_quote_char)
 	while (*lexer->right != left_quote_char)
 	{
 		if (*lexer->right == '\0')
-			ft_printf("Syntax error: quote not closed");
+			exit(ft_printf("Syntax error: quote not closed"));
 		lexer->right++;
 	}
 }
