d1119f3063dcda83d1afbd20e1d6f76cfddde566
yliu <yliu@XLIJFELKNIX.local>
2024-08-31 17:54:59 +0900

    Norminette

--- src/lexer/process_operator.c
+++ src/lexer/process_operator.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 15:25:37 by yliu              #+#    #+#             */
-/*   Updated: 2024/08/31 17:46:12 by yliu             ###   ########.fr       */
+/*   Updated: 2024/08/31 17:50:06 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,32 @@ static void	update_lexer(t_lexer *lexer, t_token_type type, size_t length)
 	discards_chars(lexer, length);
 }
 
+static void	process_operator_type(t_lexer *lexer)
+{
+	if (ft_strncmp("&&", lexer->right, 2) == 0)
+		update_lexer(lexer, TOKEN_AND_IF, 2);
+	else if (ft_strncmp("||", lexer->right, 2) == 0)
+		update_lexer(lexer, TOKEN_OR_IF, 2);
+	else if (ft_strncmp("(", lexer->right, 1) == 0)
+		update_lexer(lexer, TOKEN_L_PARENTHESIS, 1);
+	else if (ft_strncmp(")", lexer->right, 1) == 0)
+		update_lexer(lexer, TOKEN_R_PARENTHESIS, 1);
+	else if (ft_strncmp("|", lexer->right, 1) == 0)
+		update_lexer(lexer, TOKEN_PIPE, 1);
+	else if (ft_strncmp("<<", lexer->right, 2) == 0)
+		update_lexer(lexer, TOKEN_DLESS, 2);
+	else if (ft_strncmp("<", lexer->right, 1) == 0)
+		update_lexer(lexer, TOKEN_LESS, 1);
+	else if (ft_strncmp(">>", lexer->right, 2) == 0)
+		update_lexer(lexer, TOKEN_DGREAT, 2);
+	else if (ft_strncmp(">", lexer->right, 1) == 0)
+		update_lexer(lexer, TOKEN_GREAT, 1);
+}
+
 void	process_operator(t_lexer *lexer)
 {
 	if (lexer->right == lexer->left)
-	{
-		if (ft_strncmp("&&", lexer->right, 2) == 0)
-			update_lexer(lexer, TOKEN_AND_IF, 2);
-		else if (ft_strncmp("||", lexer->right, 2) == 0)
-			update_lexer(lexer, TOKEN_OR_IF, 2);
-		else if (ft_strncmp("(", lexer->right, 1) == 0)
-			update_lexer(lexer, TOKEN_L_PARENTHESIS, 1);
-		else if (ft_strncmp(")", lexer->right, 1) == 0)
-			update_lexer(lexer, TOKEN_R_PARENTHESIS, 1);
-		else if (ft_strncmp("|", lexer->right, 1) == 0)
-			update_lexer(lexer, TOKEN_PIPE, 1);
-		else if (ft_strncmp("<<", lexer->right, 2) == 0)
-			update_lexer(lexer, TOKEN_DLESS, 2);
-		else if (ft_strncmp("<", lexer->right, 1) == 0)
-			update_lexer(lexer, TOKEN_LESS, 1);
-		else if (ft_strncmp(">>", lexer->right, 2) == 0)
-			update_lexer(lexer, TOKEN_DGREAT, 2);
-		else if (ft_strncmp(">", lexer->right, 1) == 0)
-			update_lexer(lexer, TOKEN_GREAT, 1);
-	}
+		process_operator_type(lexer);
 	else
 	{
 		lexer->value = ft_strndup(lexer->left, lexer->right - lexer->left);
