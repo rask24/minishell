397ffa0a542fcc727ffcaabded190f2f8691e3ad
rask24 <rask0x18@gmail.com>
2024-09-03 17:58:13 +0900

    Replace perror with print_error in ui

--- src/ui/reset_signal_handlers.c
+++ src/ui/reset_signal_handlers.c
@@ -6,17 +6,21 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 20:03:51 by reasuke           #+#    #+#             */
-/*   Updated: 2024/08/27 00:35:26 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/03 17:52:48 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <errno.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
+
+#include "utils.h"
 
 void	reset_signal_handlers(void)
 {
 	if (signal(SIGINT, SIG_DFL) == SIG_ERR)
-		perror("signal");
+		print_error("signal", strerror(errno));
 	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
-		perror("signal");
+		print_error("signal", strerror(errno));
 }
