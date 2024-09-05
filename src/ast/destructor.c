fcc9c5f5edabd9786a30d91275ad1d2733652564
rask24 <rask0x18@gmail.com>
2024-09-05 23:12:07 +0900

    Fix typo

--- src/ast/destructor.c
+++ src/ast/destructor.c
@@ -6,14 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 18:58:00 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/05 19:13:22 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/05 23:10:51 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 #include "libft.h"
 
-static void	_destory_redirect_info(void *content)
+static void	_destroy_redirect_info(void *content)
 {
 	t_redirect_info	*info;
 
@@ -27,7 +27,7 @@ void	destroy_ast(t_ast *ast)
 	if (ast == NULL)
 		return ;
 	ft_lstclear(&ast->cmd_args, free);
-	ft_lstclear(&ast->redirects, _destory_redirect_info);
+	ft_lstclear(&ast->redirects, _destroy_redirect_info);
 	destroy_ast(ast->left);
 	destroy_ast(ast->right);
 }
