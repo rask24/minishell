525e3b5593dd100f22a7efd77c955fbb71ac5f0e
rask24 <rask0x18@gmail.com>
2024-09-05 19:36:48 +0900

    Update push_*

--- src/ast/push.c
+++ src/ast/push.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 18:13:36 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/05 19:24:42 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/05 19:36:22 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	push_cmd_arg(t_ast *ast, const char *arg)
 	if (ast->type != AST_COMMAND)
 	{
 		print_error(__func__, "invalid node type");
-		exit(EXIT_FAILURE);
+		return ;
 	}
 	ft_lstadd_back(&ast->cmd_args, ft_xlstnew(ft_xstrdup(arg)));
 }
@@ -30,7 +30,7 @@ void	push_redirect_info(t_ast *ast, t_redirect_info *info)
 	if (ast->type != AST_COMMAND)
 	{
 		print_error(__func__, "invalid node type");
-		exit(EXIT_FAILURE);
+		return ;
 	}
 	ft_lstadd_back(&ast->redirects,
 		ft_lstnew(construct_redirect_info(info->type, info->filename)));
