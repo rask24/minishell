6cdfd7bd8d263c884fc43d1905b56b9a65863f02
rask24 <rask0x18@gmail.com>
2024-09-06 21:27:01 +0900

    Implement consume_token

--- src/parser/consume_token.c
+++ src/parser/consume_token.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_internal.h                                  :+:      :+:    :+:   */
+/*   consume_token.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/06 19:22:57 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/06 20:00:06 by reasuke          ###   ########.fr       */
+/*   Created: 2024/09/06 19:55:24 by reasuke           #+#    #+#             */
+/*   Updated: 2024/09/06 21:25:32 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_INTERNAL_H
-# define PARSER_INTERNAL_H
+#include <stdbool.h>
 
-# include <stdbool.h>
+#include "ast.h"
+#include "token.h"
 
-# include "ast.h"
-# include "token.h"
-
-t_ast	*parse_simple_command(t_token_list *token_list);
-bool	consume_token(t_token_list **token_list, t_token_type type);
-
-#endif
+bool	consume_token(t_token_list **token_list, t_token_type type)
+{
+	if (*token_list == NULL || get_token_type(*token_list) != type)
+		return (false);
+	*token_list = (*token_list)->next;
+	return (true);
+}
--- tests/unit/test_parser.cpp
+++ tests/unit/test_parser.cpp
@@ -1,14 +1,33 @@
 // Copyright 2024, reasuke
 
+#include <cstring>
+
 #include "gtest/gtest.h"
 
 extern "C" {
+#include "libft.h"
 #include "parser.h"
 #include "parser/parser_internal.h"
+#include "token.h"
 }
 
-// TODO: delete
-TEST(parser, ExampleTest) {
-  t_ast *ast = parser(nullptr);
-  EXPECT_EQ(ast, nullptr);
+TEST(consume_token, MultipleTokens) {
+  t_token_list *token_list = nullptr;
+
+  ft_lstadd_back(&token_list, construct_token(TOKEN_WORD, strdup("ls")));
+  ft_lstadd_back(&token_list, construct_token(TOKEN_WORD, strdup("-l")));
+  ft_lstadd_back(&token_list, construct_token(TOKEN_WORD, strdup("src")));
+
+  EXPECT_EQ(get_token_type(token_list), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(token_list), "ls");
+  EXPECT_EQ(consume_token(&token_list, TOKEN_WORD), true);
+  EXPECT_EQ(get_token_type(token_list), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(token_list), "-l");
+  EXPECT_EQ(consume_token(&token_list, TOKEN_WORD), true);
+  EXPECT_EQ(get_token_type(token_list), TOKEN_WORD);
+  EXPECT_STREQ(get_token_value(token_list), "src");
+  EXPECT_EQ(consume_token(&token_list, TOKEN_WORD), true);
+  EXPECT_EQ(token_list, nullptr);
+
+  destroy_token_list(token_list);
 }
