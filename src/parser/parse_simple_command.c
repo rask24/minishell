4b5a2efee07ddcaba33fb44c5d8e8221a3bbb683
rask24 <rask0x18@gmail.com>
2024-09-07 02:43:46 +0900

    Implement parse_simple_command

--- src/parser/parse_simple_command.c
+++ src/parser/parse_simple_command.c
@@ -6,18 +6,49 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 19:26:37 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/06 19:29:21 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/07 02:37:20 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
+
 #include "ast.h"
+#include "parser_internal.h"
 #include "token.h"
 
-t_ast	*parse_simple_command(t_token_list *token_list)
+bool	_is_redirect_token(t_token_type type)
 {
-	t_ast	*ast;
+	return (type == TOKEN_LESS
+		|| type == TOKEN_GREAT
+		|| type == TOKEN_DLESS
+		|| type == TOKEN_DGREAT);
+}
 
-	(void)token_list;
-	ast = NULL;
+// TODO: Replace the second argument of push_redirect_info
+t_ast	*parse_simple_command(t_token_list **cur_token)
+{
+	t_ast			*ast;
+	t_token_type	token_type;
+	const char		*filepath;
+
+	(void)cur_token;
+	ast = construct_ast(AST_COMMAND, NULL, NULL);
+	while (get_token_type(*cur_token) != TOKEN_EOF)
+	{
+		if (get_token_type(*cur_token) == TOKEN_WORD)
+		{
+			push_cmd_arg(ast, get_token_value(*cur_token));
+			expect_token(cur_token, TOKEN_WORD);
+		}
+		else if (_is_redirect_token(get_token_type(*cur_token)))
+		{
+			token_type = get_token_type(*cur_token);
+			expect_token(cur_token, token_type);
+			filepath = get_token_value(*cur_token);
+			expect_token(cur_token, TOKEN_WORD);
+			push_redirect_info(ast,
+				&(t_redirect_info){(t_redirect_type)token_type, filepath});
+		}
+	}
 	return (ast);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 19:11:50 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/06 19:29:31 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/06 21:31:37 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 
 t_ast	*parser(t_token_list *token_list)
 {
-	t_ast	*ast;
+	t_ast			*ast;
+	t_token_list	**cur_token;
 
-	ast = parse_simple_command(token_list);
+	cur_token = &token_list;
+	ast = parse_simple_command(cur_token);
 	return (ast);
 }
