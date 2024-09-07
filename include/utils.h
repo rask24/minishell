a17491471f31aa4369551c27d1ab7562c46d35f0
yliu <yliu@XLIJFELKNIX.local>
2024-09-07 16:44:02 +0900

    Add lst helper func

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 22:27:21 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/05 18:19:11 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/07 16:46:15 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@
 # include <stddef.h>
 
 # include "libft.h"
+# include "stdlib.h"
+
+typedef bool	(*t_pred)(t_list *node);
 
 void	print_error(const char *func, const char *desc);
 
 t_list	*ft_xlstnew(void *content);
 char	*ft_xstrndup(const char *s, size_t n);
 char	*ft_xstrdup(const char *s);
+
+void	ft_lstremove_if(t_list **lst, t_pred is_remove, void (*del)(void *));
 
 #endif
