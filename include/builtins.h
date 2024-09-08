d8a3bf40d9e0c0a9db6e43f0ec8b904d120f5e7c
yliu <yliu@XLIJFELKNIX.local>
2024-09-08 15:40:55 +0900

    Impl exit command

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:44:57 by yliu              #+#    #+#             */
-/*   Updated: 2024/09/11 11:36:17 by yliu             ###   ########.fr       */
+/*   Updated: 2024/09/11 11:39:14 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,23 @@
 # include "libft.h"
 # include "utils.h"
 
-int	builtins_echo(char **args, char **env);
-int	builtins_pwd(char **args, char **env);
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+	struct s_env	*prev;
+}					t_env;
+
+typedef struct s_config
+{
+	char			*cwd;
+	t_env			*env;
+	int				exit_status;
+}					t_config;
+
+int					builtins_echo(char **args, char **env);
+int					builtins_exit(char **args, t_config *config);
+int					builtins_pwd(char **args, char **env);
 
 #endif
