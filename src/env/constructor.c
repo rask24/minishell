6d3f854c0549b39f40cab9dab689221881b35790
yliu <yliu@XLIJFELKNIX.local>
2024-09-09 15:23:58 +0900

    Fix constructor

--- src/env/constructor.c
+++ src/env/constructor.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 12:31:39 by yliu              #+#    #+#             */
-/*   Updated: 2024/09/08 14:22:23 by yliu             ###   ########.fr       */
+/*   Updated: 2024/09/09 15:23:30 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ t_env_list	*construct_env(char *name, char *value)
 	t_env		*content;
 
 	content = (t_env *)ft_xmalloc(sizeof(t_env));
-	content->name = ft_xstrdup(name);
-	content->value = ft_xstrdup(value);
+	content->name = name;
+	content->value = value;
 	env = ft_xlstnew(content);
 	return (env);
 }
