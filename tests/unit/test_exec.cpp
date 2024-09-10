db4fefffcb9ff1b7bca4bc5ed6fd3cf559ae4347
yliu <yliu@XLIJFELKNIX.local>
2024-09-10 17:15:10 +0900

    Fix exec test

--- tests/unit/test_exec.cpp
+++ tests/unit/test_exec.cpp
@@ -4,49 +4,12 @@ extern "C" {
 #include "exec.h"
 }
 
-typedef void (*func)(char *command, char **envp);
-
-static char *test_helper(func f, char *command, char **envp) {
-  int pipefd[2];
-  pipe(pipefd);
-  if (errno) {
-    return (NULL);
-  }
-  int write_fd = pipefd[1];
-  int read_fd = pipefd[0];
-
-  int in_fd = dup(STDOUT_FILENO);
-  if (errno) {
-    return (NULL);
-  }
-  dup2(write_fd, STDOUT_FILENO);
-  if (errno) {
-    return (NULL);
-  }
-
-  f(command, envp);
-
-  char *buf = (char *)malloc(100);
-  if (errno) {
-    return (NULL);
-  }
-  read(read_fd, buf, 100);
-  if (errno) {
-    return (NULL);
-  }
-
-  dup2(in_fd, STDOUT_FILENO);
-  if (errno) {
-    return (NULL);
-  }
-
-  close(read_fd);
-  close(write_fd);
-  return (buf);
-}
-
 TEST(exec, OneWord) {
   char *str = strdup("echo 'hello'");
-  char *answer = test_helper(spawn_process, str, NULL);
-  EXPECT_STREQ(answer, "hello\n");
+
+  testing::internal::CaptureStdout();
+  spawn_process(str, NULL);
+
+  EXPECT_STREQ(testing::internal::GetCapturedStdout().c_str(), "hello\n");
+  (void)str;
 }
