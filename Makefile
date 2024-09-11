52eb10e5bcb2449b619e24fcd108582b444d6049
yliu <yliu@XLIJFELKNIX.local>
2024-09-11 10:39:39 +0900

    Impl env command

--- Makefile
+++ Makefile
@@ -27,6 +27,7 @@ SRC				= $(SRC_DIR)/main.c \
 					$(SRC_DIR)/ast/push.c \
 					$(SRC_DIR)/exec/exec.c \
 					$(SRC_DIR)/builtins/builtins_echo.c \
+					$(SRC_DIR)/builtins/builtins_env.c \
 					$(SRC_DIR)/builtins/builtins_exit.c \
 					$(SRC_DIR)/builtins/builtins_pwd.c \
 					$(SRC_DIR)/builtins/builtins_unset.c \
---
+++ tests/unit/test_builtins_env.cpp
@@ -0,0 +1,25 @@
+#include <cstring>
+
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "builtins.h"
+}
+
+TEST(builtins_env, builtin_env) {
+  t_env_list *env = construct_env(strdup("key"), strdup("value"));
+  t_env_list *env1 = construct_env(strdup("key1"), strdup("value1"));
+  t_env_list *env2 = construct_env(strdup("key2"), strdup("value2"));
+  ft_lstadd_back(&env, env1);
+  ft_lstadd_back(&env, env2);
+
+  t_config config;
+  config.env = env;
+
+  testing::internal::CaptureStdout();
+  int result = builtins_env(NULL, &config);
+  std::string output = testing::internal::GetCapturedStdout();
+
+  EXPECT_STREQ(output.c_str(), "key=value\nkey1=value1\nkey2=value2\n");
+  EXPECT_EQ(result, 0);
+}
