f51e908cc4595a1f1ef1607392190b750c7c373a
yliu <yliu@XLIJFELKNIX.local>
2024-09-13 16:25:14 +0900

    Add NULL guards

--- src/env/getter.c
+++ src/env/getter.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 13:16:13 by yliu              #+#    #+#             */
-/*   Updated: 2024/09/13 16:18:12 by yliu             ###   ########.fr       */
+/*   Updated: 2024/09/13 16:24:59 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 char	*get_env_name(t_env_list *env)
 {
+	if (env == NULL)
+		return (NULL);
 	return (((t_env *)env->content)->key);
 }
 
 char	*get_env_value(t_env_list *env)
 {
+	if (env == NULL)
+		return (NULL);
 	return (((t_env *)env->content)->value);
 }
