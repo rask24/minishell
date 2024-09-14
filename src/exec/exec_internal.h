11d207f88a087b95fe1e3ece857f5ac8f51721bc
rask24 <rask0x18@gmail.com>
2024-09-14 23:34:51 +0900

    Replace ast with node in exec

--- src/exec/exec_internal.h
+++ src/exec/exec_internal.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 17:46:21 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/14 17:02:28 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/14 23:33:41 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "libft.h"
 
-pid_t	execute_pipeline(t_ast *ast, char **envp, int fd_in, int fd_out);
+pid_t	execute_pipeline(t_ast *node, char **envp, int fd_in, int fd_out);
 pid_t	execute_simple_command(t_ast *cmd_node, char **envp,
 			int fd_int, int fd_out);
 int		handle_redirects(t_list *redirects);
