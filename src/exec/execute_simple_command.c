c7b83a073f14a4cfea0b616fb2b66878627b005c
rask24 <rask0x18@gmail.com>
2024-09-14 17:06:13 +0900

    Add error handling for handle_pipeline

--- src/exec/execute_simple_command.c
+++ src/exec/execute_simple_command.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 16:18:33 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/14 17:02:18 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/14 17:05:16 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,27 @@ static char	**convert_list_to_array(t_list *cmd_args)
 	return (argv);
 }
 
-static void	handle_pipeline(int fd_in, int fd_out)
+static int	handle_pipeline(int fd_in, int fd_out)
 {
 	if (fd_in != STDIN_FILENO)
 	{
 		if (dup2(fd_in, STDIN_FILENO) == -1)
+		{
 			print_error("dup2", strerror(errno));
+			return (-1);
+		}
 		close(fd_in);
 	}
 	if (fd_out != STDOUT_FILENO)
 	{
 		if (dup2(fd_out, STDOUT_FILENO) == -1)
+		{
 			print_error("dup2", strerror(errno));
+			return (-1);
+		}
 		close(fd_out);
 	}
+	return (0);
 }
 
 pid_t	execute_simple_command(t_ast *cmd_node, char **envp,
@@ -68,7 +75,8 @@ pid_t	execute_simple_command(t_ast *cmd_node, char **envp,
 	if (pid == 0)
 	{
 		argv = convert_list_to_array(cmd_node->cmd_args);
-		handle_pipeline(fd_in, fd_out);
+		if (handle_pipeline(fd_in, fd_out) == -1)
+			exit(EXIT_FAILURE);
 		if (handle_redirects(cmd_node->redirects) == -1)
 			exit(EXIT_FAILURE);
 		reset_signal_handlers();
