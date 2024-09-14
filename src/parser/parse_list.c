bf34209989eb5479f3dacd2ddac7a86a9eb5307e
rask24 <rask0x18@gmail.com>
2024-09-14 23:24:05 +0900

    Remove underscore from static functions

--- src/parser/parse_list.c
+++ src/parser/parse_list.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 20:48:52 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/14 21:06:27 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/14 23:23:40 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "parser_internal.h"
 #include "token.h"
 
-static t_ast_node_type	_convert_token_to_node(t_token_type token_type)
+static t_ast_node_type	convert_token_to_node(t_token_type token_type)
 {
 	if (token_type == TOKEN_AND_IF)
 		return (AST_AND);
@@ -39,7 +39,7 @@ t_ast	*parse_list(t_token_list **cur_token)
 		return (NULL);
 	while (get_token_type(*cur_token) != TOKEN_EOF)
 	{
-		node_type = _convert_token_to_node(get_token_type(*cur_token));
+		node_type = convert_token_to_node(get_token_type(*cur_token));
 		if (node_type != AST_AND && node_type != AST_OR)
 			return (handle_error(node, get_token_value(*cur_token)));
 		consume_token(cur_token);
