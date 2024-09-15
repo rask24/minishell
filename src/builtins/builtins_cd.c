f5ec6b2d9aa70d1de663f9516df83a38100e50ce
yliu <yliu@XLIJFELKNIX.local>
2024-09-15 23:10:27 +0900

    Impl cd command

---
+++ src/builtins/builtins_cd.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_cd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 20:37:58 by yliu              #+#    #+#             */
+/*   Updated: 2024/09/15 23:11:40 by yliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+static int	move_to_home(void)
+{
+	char	*home_dir;
+	int		ret;
+
+	home_dir = getenv("HOME");
+	ret = chdir(home_dir);
+	if (ret != 0)
+		return (ret);
+	return (0);
+}
+
+static char	*join_path(const char *cwd, const char *dirname)
+{
+	int		i;
+	char	*tmp;
+	char	*tmp2;
+
+	i = 0;
+	while (cwd[i])
+		i++;
+	if (i == 0)
+		return (ft_xstrdup(dirname));
+	else if (cwd[i - 1] == '/')
+		tmp = ft_xstrdup(cwd);
+	else
+		tmp = ft_xstrjoin(cwd, "/");
+	tmp2 = ft_xstrjoin(tmp, dirname);
+	free(tmp);
+	return (tmp2);
+}
+
+int	builtins_cd(char **args, t_config *config)
+{
+	char	*dirname;
+	char	*fullpath;
+	int		res;
+
+	if (args[1] == NULL)
+		return (move_to_home());
+	dirname = args[1];
+	if (dirname[0] == '/')
+		fullpath = dirname;
+	else
+		fullpath = join_path(config->cwd, dirname);
+	res = chdir(fullpath);
+	if (res == 0)
+	{
+		free(config->cwd);
+		config->cwd = fullpath;
+	}
+	return (res);
+}
---
+++ tests/unit/test_builtins_cd.cpp
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+
+#include <cstring>
+
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "../../include/builtins.h"
+}
+
+// TEST(join_path, RelativePath) {
+//   const char *ans = join_path(getenv("HOME"), "tests");
+//   EXPECT_STREQ(ans, ft_strjoin(getenv("HOME"), "/tests"));
+// }
+
+// TEST(join_path, RelativePath2) {
+//   const char *ans = join_path("/", "bin/");
+//   EXPECT_STREQ(ans, "/bin/");
+// }
+
+TEST(builtins_cd, WithoutArgs) {
+  char *args[] = {strdup("cd"), NULL};
+
+  builtins_cd(args, NULL);
+  char *expected = getcwd(NULL, 0);
+
+  EXPECT_STREQ(expected, getenv("HOME"));
+}
