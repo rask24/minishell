69e57a971e73acc55dbcf234fb92e6dbc6aba8f9
yliu <yliu@XLIJFELKNIX.local>
2024-09-20 22:42:17 +0900

    Fix env to only display valid key, and add error management

--- src/builtins/builtins_env.c
+++ src/builtins/builtins_env.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 14:41:44 by yliu              #+#    #+#             */
-/*   Updated: 2024/09/20 12:09:39 by yliu             ###   ########.fr       */
+/*   Updated: 2024/09/20 22:33:19 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 static void	print_env_entry(void *content)
 {
-	printf("%s=%s\n", ((t_env *)content)->key, ((t_env *)content)->value);
+	if (((t_env *)content)->is_valid_value)
+		printf("%s=%s\n", ((t_env *)content)->key, ((t_env *)content)->value);
 }
 
 int	builtins_env(char **args, t_builtins_ctx *ctx)
 {
-	(void)args;
+	if (args[1])
+	{
+		print_error("env", "too many arguments");
+		return (EXIT_FAILURE);
+	}
 	ft_lstiter(ctx->env, print_env_entry);
 	return (EXIT_SUCCESS);
 }
--- tests/unit/test_builtins_env.cpp
+++ tests/unit/test_builtins_env.cpp
@@ -16,10 +16,30 @@ TEST(builtins_env, builtin_env) {
   t_builtins_ctx config;
   config.env = env;
 
+  char *args[] = {ft_strdup("env"), NULL};
   testing::internal::CaptureStdout();
-  int result = builtins_env(NULL, &config);
+  int result = builtins_env(args, &config);
   std::string output = testing::internal::GetCapturedStdout();
 
   EXPECT_STREQ(output.c_str(), "key=value\nkey1=value1\nkey2=value2\n");
   EXPECT_EQ(result, 0);
+}
+
+TEST(builtins_env, moreArgs) {
+  t_env_list *env = construct_env(strdup("key"), strdup("value"));
+  t_env_list *env1 = construct_env(strdup("key1"), strdup("value1"));
+  t_env_list *env2 = construct_env(strdup("key2"), strdup("value2"));
+  ft_lstadd_back(&env, env1);
+  ft_lstadd_back(&env, env2);
+
+  t_builtins_ctx config;
+  config.env = env;
+
+  char *args[] = {ft_strdup("env"), ft_strdup("key3=value3"), NULL};
+  testing::internal::CaptureStderr();
+  int result = builtins_env(args, &config);
+  std::string output = testing::internal::GetCapturedStderr();
+
+  EXPECT_STREQ(output.c_str(), "minishell: env: too many arguments\n");
+  EXPECT_EQ(result, 1);
 }
