3855e1ce24493382904f48549b464b01ffa79bf4
rask24 <rask0x18@gmail.com>
2024-09-20 14:51:29 +0900

    Replace envp with env_list in exec

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 12:54:34 by yliu              #+#    #+#             */
-/*   Updated: 2024/09/14 23:33:24 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/20 14:40:42 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #include <unistd.h>
 
 #include "ast.h"
+#include "env.h"
 #include "exec_internal.h"
 #include "lexer.h"
 #include "parser.h"
 #include "utils.h"
 
-void	exec(char *input, char **envp)
+void	exec(char *input, t_env_list *env_list)
 {
 	t_token_list	*token_list;
 	t_ast			*node;
@@ -37,7 +38,7 @@ void	exec(char *input, char **envp)
 	}
 	if (node->type == AST_PIPE || node->type == AST_COMMAND)
 	{
-		last_pid = execute_pipeline(node, envp, STDIN_FILENO, STDOUT_FILENO);
+		last_pid = execute_pipeline(node, env_list, STDIN_FILENO, STDOUT_FILENO);
 		waitpid(last_pid, &status, 0);
 		while (waitpid(-1, NULL, 0) > 0)
 			;
--- src/exec/execute_pipeline.c
+++ src/exec/execute_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 16:15:45 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/14 23:33:50 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/20 14:41:22 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@
 #define PIPE_IN 0
 #define PIPE_OUT 1
 
-pid_t	execute_pipeline(t_ast *node, char **envp, int fd_in, int fd_out)
+pid_t	execute_pipeline(t_ast *node, t_env_list *env_list, int fd_in, int fd_out)
 {
 	int				pipe_fds[2];
 	pid_t			pid;
 
 	if (node->type == AST_COMMAND)
-		return (execute_simple_command(node, envp, fd_in, fd_out));
+		return (execute_simple_command(node, env_list, fd_in, fd_out));
 	if (pipe(pipe_fds) == -1)
 	{
 		print_error("pipe", strerror(errno));
 		return (-1);
 	}
-	execute_pipeline(node->left, envp, fd_in, pipe_fds[PIPE_OUT]);
+	execute_pipeline(node->left, env_list, fd_in, pipe_fds[PIPE_OUT]);
 	close(pipe_fds[PIPE_OUT]);
-	pid = execute_pipeline(node->right, envp, pipe_fds[PIPE_IN], fd_out);
+	pid = execute_pipeline(node->right, env_list, pipe_fds[PIPE_IN], fd_out);
 	close(pipe_fds[PIPE_IN]);
 	return (pid);
 }
