692fed7442b847cd6565e0062aed0f4ba1f905de
rask24 <rask0x18@gmail.com>
2024-09-25 15:18:43 +0900

    Add e2e test for many pipes

--- tests/e2e/test_pipeline.py
+++ tests/e2e/test_pipeline.py
@@ -34,3 +34,14 @@ def test_pipeline_with_redirection(shell_session):
     finally:
         if os.path.exists(test_file):
             os.remove(test_file)
+
+
+def test_pipeline_many_commands(shell_session):
+    pipe_command = "echo Hello"
+    for _ in range(1000):
+        pipe_command += " | cat"
+    shell_session.sendline(pipe_command)
+    shell_session.expect(PROMPT)
+
+    result = get_command_output(shell_session.before)
+    assert "Hello" == result
