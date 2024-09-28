52688bbe7c2adb1c94576e28930bcdfe0d8b5448
yliu <yliu@XLIJFELKNIX.local>
2024-09-28 15:37:33 +0900

    Fix memory leak, and add redirect for builtin func

--- src/exec/execute_command.c
+++ src/exec/execute_command.c
@@ -12,26 +12,85 @@
 
 #include "exec_internal.h"
 
-int	execute_command(t_ast *node, t_ctx *ctx, t_pipeline_conf *conf)
+static bool	is_builtin(char *cmd_name)
+{
+	if (ft_strcmp(cmd_name, "echo") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "cd") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "pwd") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "export") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "unset") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "env") == 0)
+		return (true);
+	if (ft_strcmp(cmd_name, "exit") == 0)
+		return (true);
+	return (false);
+}
+
+static void	push_io(int *fd_array)
+{
+	fd_array[0] = dup(STDIN_FILENO);
+	fd_array[1] = dup(STDOUT_FILENO);
+	fd_array[2] = dup(STDERR_FILENO);
+}
+
+static void	pop_io(int *fd_array)
+{
+	dup2(fd_array[0], STDIN_FILENO);
+	dup2(fd_array[1], STDOUT_FILENO);
+	dup2(fd_array[2], STDERR_FILENO);
+	close(fd_array[0]);
+	close(fd_array[1]);
+	close(fd_array[2]);
+}
+
+static int	execute_builtin_command(t_ast *node, t_ctx *ctx)
 {
 	char	*cmd_name;
-	char	**cmd_array;
+	char	**argv;
+	int		status;
 
+	status = 0;
 	cmd_name = node->cmd_args->content;
-	cmd_array = convert_cmd_args_to_array(node->cmd_args);
+	argv = convert_cmd_args_to_array(node->cmd_args);
+	if (!argv)
+		return (EXIT_FAILURE);
 	if (ft_strcmp(cmd_name, "cd") == 0)
-		return (builtins_cd(cmd_array, ctx));
-	if (ft_strcmp(cmd_name, "pwd") == 0)
-		return (builtins_pwd(cmd_array, ctx));
-	if (ft_strcmp(cmd_name, "echo") == 0)
-		return (builtins_echo(cmd_array, ctx));
+		status = builtins_cd(argv, ctx);
+	else if (ft_strcmp(cmd_name, "pwd") == 0)
+		status = builtins_pwd(argv, ctx);
+	else if (ft_strcmp(cmd_name, "echo") == 0)
+		status = builtins_echo(argv, ctx);
 	else if (ft_strcmp(cmd_name, "env") == 0)
-		return (builtins_env(cmd_array, ctx));
+		status = builtins_env(argv, ctx);
 	else if (ft_strcmp(cmd_name, "export") == 0)
-		return (builtins_export(cmd_array, ctx));
+		status = builtins_export(argv, ctx);
 	else if (ft_strcmp(cmd_name, "unset") == 0)
-		return (builtins_unset(cmd_array, ctx));
+		status = builtins_unset(argv, ctx);
 	else if (ft_strcmp(cmd_name, "exit") == 0)
-		return (builtins_exit(cmd_array, ctx));
-	return (spawn_command(node, ctx, conf));
+		status = builtins_exit(argv, ctx);
+	ft_free_strs(argv);
+	return (status);
+}
+
+int	execute_command(t_ast *node, t_ctx *ctx, t_pipeline_conf *conf)
+{
+	int	status;
+	int	fd_array[3];
+
+	status = 0;
+	if (is_builtin(node->cmd_args->content))
+	{
+		push_io(fd_array);
+		handle_io(conf, node->redirects);
+		status = execute_builtin_command(node, ctx);
+		pop_io(fd_array);
+		return (status);
+	}
+	else
+		return (spawn_command(node, ctx, conf));
 }
