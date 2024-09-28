e626b31a7f3bcc38671ab87be1b7a6e9f00ce08b
yliu <yliu@XLIJFELKNIX.local>
2024-09-28 14:44:05 +0900

    Fix pwd test

--- tests/unit/test_builtins_pwd.cpp
+++ tests/unit/test_builtins_pwd.cpp
@@ -10,9 +10,9 @@ extern "C" {
 
 TEST(builtins_pwd, WithoutArgs) {
   char *args[] = {strdup("pwd"), NULL};
-  char *env[] = {NULL};
+  t_ctx ctx;
   testing::internal::CaptureStdout();
-  int res = builtins_pwd(args, env);
+  int res = builtins_pwd(args, &ctx);
   EXPECT_EQ(res, 0);
   EXPECT_STREQ(testing::internal::GetCapturedStdout().c_str(),
                ft_strjoin(getcwd(NULL, 0), "\n"));
