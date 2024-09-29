aa5536acad9842e1caa78c80f3c1003f7c50cdcd
rask24 <rask0x18@gmail.com>
2024-09-29 16:31:49 +0900

    Refactor handle_heredoc

--- src/parser/handle_heredoc.c
+++ src/parser/handle_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:38:07 by reasuke           #+#    #+#             */
-/*   Updated: 2024/09/28 16:02:30 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/09/29 16:30:47 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,6 @@
 #include "parser_internal.h"
 #include "readline.h"
 #include "utils.h"
-
-static int	open_heredoc_tmpfile(void)
-{
-	int		fd;
-	char	*paths[5];
-	char	*path;
-	int		i;
-
-	paths[0] = "/tmp/";
-	paths[1] = "/var/tmp/";
-	paths[2] = "/usr/tmp/";
-	paths[3] = "./";
-	paths[4] = NULL;
-	i = 0;
-	while(paths[i])
-	{
-		path = ft_xstrjoin(paths[i], HEREDOC_TMPFILE);
-		fd = open(path, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-		free(path);
-		if (fd != -1)
-			break ;
-		i++;
-	}
-	return (fd);
-}
 
 static void	write_heredoc(int fd, t_list *input_list)
 {
@@ -54,12 +29,52 @@ static void	write_heredoc(int fd, t_list *input_list)
 	ft_lstclear(&input_list, free);
 }
 
+static int	create_heredoc_tmpfile(void)
+{
+	int		fd;
+	char	*paths[5];
+	char	*path;
+	int		i;
+
+	paths[0] = "/tmp/";
+	paths[1] = "/var/tmp/";
+	paths[2] = "/usr/tmp/";
+	paths[3] = "./";
+	paths[4] = NULL;
+	i = 0;
+	while (paths[i])
+	{
+		path = ft_xstrjoin(paths[i], HEREDOC_TMPFILE);
+		fd = open(path, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+		free(path);
+		if (fd != -1)
+			break ;
+		i++;
+	}
+	return (fd);
+}
+
+static int	open_heredoc_tmpfile(t_list *input_list)
+{
+	int	fd;
+
+	fd = create_heredoc_tmpfile();
+	if (fd == -1)
+		return (-1);
+	write_heredoc(fd, input_list);
+	close(fd);
+	fd = open(HEREDOC_TMPFILE, O_RDONLY);
+	unlink(HEREDOC_TMPFILE);
+	return (fd);
+}
+
 static int	open_heredoc(t_list *input_list, size_t heredoc_size)
 {
 	int	pipe_fd[2];
-	int	fd;
 
-	if (heredoc_size <= HEREDOC_PIPESIZE)
+	if (heredoc_size == 0)
+		return (open("/dev/null", O_RDONLY));
+	else if (heredoc_size <= HEREDOC_PIPESIZE)
 	{
 		if (pipe(pipe_fd) == -1)
 		{
@@ -71,16 +86,7 @@ static int	open_heredoc(t_list *input_list, size_t heredoc_size)
 		return (pipe_fd[0]);
 	}
 	else
-	{
-		fd = open_heredoc_tmpfile();
-		if (fd == -1)
-			return (-1);
-		write_heredoc(fd, input_list);
-		close(fd);
-		fd = open(HEREDOC_TMPFILE, O_RDONLY);
-		unlink(HEREDOC_TMPFILE);
-		return (fd);
-	}
+		return (open_heredoc_tmpfile(input_list));
 }
 
 int	handle_heredoc(const char *delimiter)
@@ -105,7 +111,7 @@ int	handle_heredoc(const char *delimiter)
 			break ;
 		}
 		heredoc_size += ft_strlen(line) + 1;
-		ft_lstadd_back(&input_list, ft_xlstnew(line));
+		ft_lstadd_back(&input_list, ft_xlstnew(ft_xstrdup(line)));
 	}
 	return (open_heredoc(input_list, heredoc_size));
 }
