950463db0c90c06bd591e47bef092a973ed03768
yliu <yliu@XLIJFELKNIX.local>
2024-10-06 16:58:19 +0900

    Fix wildcard order

--- src/expansion/expand_wildcard_on_list.c
+++ src/expansion/expand_wildcard_on_list.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 23:45:09 by yliu              #+#    #+#             */
-/*   Updated: 2024/10/05 15:03:06 by yliu             ###   ########.fr       */
+/*   Updated: 2024/10/06 16:56:03 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,17 @@ static bool	should_remove(t_list *file, void *wildcard_exp)
 	return (!wildcard_lazy_match(file->content, wildcard_exp));
 }
 
+static bool	simple_cmp(t_list *a, t_list *b)
+{
+	return (ft_strcmp(a->content, b->content) > 0);
+}
+
 static t_list	*expand_wildcard(char *wildcard)
 {
 	t_list	*files;
 
 	files = initialize_files();
+	ft_lstsort(&files, simple_cmp);
 	ft_lstremove_if(&files, should_remove, wildcard, free);
 	if (files == NULL)
 		return (ft_xlstnew(ft_xstrdup(wildcard)));
