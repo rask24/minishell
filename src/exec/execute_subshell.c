18b4b07b093aafa33e48fa99b2c69e155dfeedcd
rask24 <rask0x18@gmail.com>
2024-10-16 17:38:37 +0900

    Fix wait in execute_subshell

--- src/exec/execute_subshell.c
+++ src/exec/execute_subshell.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:29:00 by reasuke           #+#    #+#             */
-/*   Updated: 2024/10/16 17:20:18 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/10/16 17:37:11 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,10 @@ int	execute_subshell(t_ast *node, t_ctx *ctx, t_pipe_conf *conf)
 	}
 	if (pid == 0)
 	{
-		handle_io(NULL, node->redirects, ctx, true);
+		handle_io(conf, node->redirects, ctx, true);
 		execute_ast_node(node->left, ctx, conf);
 		exit(ctx->exit_status);
 	}
-	if (conf->next_write == STDOUT_FILENO)
-		wait_for_children_with_last_status(pid, ctx);
+	wait_for_children_with_last_status(pid, ctx);
 	return (EXIT_SUCCESS);
 }
