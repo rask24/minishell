876492b307c9b8d3a0a9e2a46511f25467a4d01c
yliu <yliu@XLIJFELKNIX.local>
2024-10-17 17:01:45 +0900

    Add SHLVL feature

--- src/env/init_env.c
+++ src/env/init_env.c
@@ -6,19 +6,37 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 21:35:41 by yliu              #+#    #+#             */
-/*   Updated: 2024/10/16 21:39:00 by yliu             ###   ########.fr       */
+/*   Updated: 2024/10/17 17:00:58 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "builtins.h"
 #include "env.h"
 #define DFLPATH "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:."
 
 static void	add_default_path(t_env_list *env_list)
 {
-	t_list		*tmp;
+	t_list	*tmp;
 
 	tmp = construct_env(ft_xstrdup("PATH"), ft_xstrdup(DFLPATH));
 	ft_lstadd_back(&env_list, tmp);
+}
+
+static void	deal_shlvl(t_env_list *env_list)
+{
+	t_list		*tmp;
+	char		*shlvl_str;
+	int			shlvl;
+
+	shlvl_str = lookup_value("SHLVL", env_list);
+	if (shlvl_str == NULL)
+	{
+		tmp = construct_env(ft_xstrdup("SHLVL"), ft_xstrdup("1"));
+		ft_lstadd_back(&env_list, tmp);
+		return ;
+	}
+	shlvl = ft_atoi(shlvl_str);
+	update_env_value(is_already_exit("SHLVL", env_list), ft_itoa(shlvl + 1));
 }
 
 t_env_list	*init_env(char **envp)
@@ -28,5 +46,6 @@ t_env_list	*init_env(char **envp)
 	env_list = convert_array_to_env(envp);
 	if (lookup_value("PATH", env_list) == NULL)
 		add_default_path(env_list);
+	deal_shlvl(env_list);
 	return (env_list);
 }
--- tests/e2e/test_subshell.py
+++ tests/e2e/test_subshell.py
@@ -1,3 +1,5 @@
+from webbrowser import get
+
 from conftest import PROMPT, get_command_output
 
 
@@ -15,3 +17,43 @@ def test_subshell_with_pipeline(shell_session):
 
     result = get_command_output(shell_session.before)
     assert "Hello$" == result
+
+
+def recursive(shell_session, i, INITIAL_SHLVL):
+    if i == 4:
+        shell_session.sendline("exit")
+        return
+
+    shell_session.sendline("./minishell")
+    shell_session.expect(PROMPT)
+    shell_session.sendline("echo $SHLVL")
+    shell_session.expect(PROMPT)
+    current_shlvl = int(get_command_output(shell_session.before))
+    assert current_shlvl == (i + INITIAL_SHLVL)
+    recursive(shell_session, i + 1, INITIAL_SHLVL)
+
+
+def test_shlvl_many_times(shell_session):
+    shell_session.sendline("echo $SHLVL")
+    shell_session.expect(PROMPT)
+    INITIAL_SHLVL = int(get_command_output(shell_session.before))
+
+    recursive(shell_session, 1, INITIAL_SHLVL)
+
+
+def test_shlvl_with_unset_shlvl(shell_session):
+    shell_session.sendline("echo $SHLVL")
+    shell_session.expect(PROMPT)
+    shell_session.sendline("unset SHLVL")
+    shell_session.expect(PROMPT)
+
+    recursive(shell_session, 1, 0)
+
+
+def test_shlvl_with_set_invalid_shlvl(shell_session):
+    shell_session.sendline("echo $SHLVL")
+    shell_session.expect(PROMPT)
+    shell_session.sendline("export SHLVL=invalid")
+    shell_session.expect(PROMPT)
+
+    recursive(shell_session, 1, 0)
