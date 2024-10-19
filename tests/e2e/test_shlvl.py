ee04c937dcb6ff227813f1a5dc5c4d60a8a3c8bf
yliu <yliu@XLIJFELKNIX.local>
2024-10-19 11:16:11 +0900

    Use lowercase to variable name

--- tests/e2e/test_shlvl.py
+++ tests/e2e/test_shlvl.py
@@ -1,7 +1,7 @@
 from conftest import PROMPT, get_command_output
 
 
-def check_shlvl_increment_recursively(shell_session, i, INITIAL_SHLVL):
+def check_shlvl_increment_recursively(shell_session, i, initial_shlvl):
     if i == 2:
         shell_session.sendline("exit")
         return
@@ -10,17 +10,21 @@ def check_shlvl_increment_recursively(shell_session, i, INITIAL_SHLVL):
     shell_session.expect(PROMPT)
     shell_session.sendline("echo $SHLVL")
     shell_session.expect(PROMPT)
-    current_shlvl = int(get_command_output(shell_session.before))
-    assert current_shlvl == (i + INITIAL_SHLVL)
-    check_shlvl_increment_recursively(shell_session, i + 1, INITIAL_SHLVL)
+
+    str = get_command_output(shell_session.before)
+    print(str)
+    current_shlvl = int(str)
+
+    assert current_shlvl == (i + initial_shlvl)
+    check_shlvl_increment_recursively(shell_session, i + 1, initial_shlvl)
 
 
 def test_shlvl_many_times(shell_session):
     shell_session.sendline("echo $SHLVL")
     shell_session.expect(PROMPT)
-    INITIAL_SHLVL = int(get_command_output(shell_session.before))
+    initial_shlvl = int(get_command_output(shell_session.before))
 
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_unset_shlvl(shell_session):
@@ -29,8 +33,8 @@ def test_shlvl_with_unset_shlvl(shell_session):
     shell_session.sendline("unset SHLVL")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_set_invalid_shlvl(shell_session):
@@ -39,8 +43,8 @@ def test_shlvl_with_set_invalid_shlvl(shell_session):
     shell_session.sendline("export SHLVL=invalid")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_set_zero(shell_session):
@@ -49,8 +53,8 @@ def test_shlvl_with_set_zero(shell_session):
     shell_session.sendline("export SHLVL=0")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_minus(shell_session):
@@ -59,8 +63,8 @@ def test_shlvl_with_minus(shell_session):
     shell_session.sendline("export SHLVL=-42")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = -1
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = -1
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_plus_overflow(shell_session):
@@ -69,8 +73,8 @@ def test_shlvl_with_plus_overflow(shell_session):
     shell_session.sendline("export SHLVL=4242424242424242424242424242424242424242424242")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_minus_overflow(shell_session):
@@ -79,8 +83,8 @@ def test_shlvl_with_minus_overflow(shell_session):
     shell_session.sendline("export SHLVL=-4242424242424242424242424242424242424242424242")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_max(shell_session):
@@ -89,8 +93,8 @@ def test_shlvl_with_max(shell_session):
     shell_session.sendline("export SHLVL=999")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 0
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 0
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_max_minus_one(shell_session):
@@ -99,8 +103,8 @@ def test_shlvl_with_max_minus_one(shell_session):
     shell_session.sendline("export SHLVL=998")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 998
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 998
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
 
 
 def test_shlvl_with_fourtytwo(shell_session):
@@ -109,5 +113,5 @@ def test_shlvl_with_fourtytwo(shell_session):
     shell_session.sendline("export SHLVL=42")
     shell_session.expect(PROMPT)
 
-    INITIAL_SHLVL = 42
-    check_shlvl_increment_recursively(shell_session, 1, INITIAL_SHLVL)
+    initial_shlvl = 42
+    check_shlvl_increment_recursively(shell_session, 1, initial_shlvl)
