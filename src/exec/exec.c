f972351e2e5fff12b2e5006a0f4c8b3ae0158300
rask24 <rask0x18@gmail.com>
2024-10-19 16:05:56 +0900

    Refactor execute_pipeline

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 12:54:34 by yliu              #+#    #+#             */
-/*   Updated: 2024/10/19 15:44:00 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/10/19 15:49:13 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	execute_ast_node(t_ast *node, t_ctx *ctx, t_pipe_conf *conf)
 	else if (node->type == AST_COMMAND)
 		return (execute_command(node, ctx, conf));
 	else if (node->type == AST_PIPE)
-		return (execute_pipeline(node, ctx, conf));
+		return (execute_pipeline(node, ctx));
 	else if (node->type == AST_AND || node->type == AST_OR)
 		return (execute_and_or(node, ctx));
 	else if (node->type == AST_SUBSHELL)
