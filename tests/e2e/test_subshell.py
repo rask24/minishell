4354d3d216dd19862501940a46d924d05546e29b
yliu <yliu@XLIJFELKNIX.local>
2024-10-22 20:58:00 +0900

    Move error test to top

--- tests/e2e/test_subshell.py
+++ tests/e2e/test_subshell.py
@@ -1,30 +1,6 @@
 from conftest import PROMPT, get_command_output
 
 
-def test_simple_subshell(shell_session):
-    shell_session.sendline("(echo Hello)")
-    shell_session.expect(PROMPT)
-
-    result = get_command_output(shell_session.before)
-    assert "Hello" == result
-
-
-def test_subshell_with_pipeline(shell_session):
-    shell_session.sendline("(echo Hello) | ((cat) | (cat -e))")
-    shell_session.expect(PROMPT)
-
-    result = get_command_output(shell_session.before)
-    assert "Hello$" == result
-
-
-def test_subshell_with_pipeline_simple(shell_session):
-    shell_session.sendline("(ls -l) | (echo Hello)")
-    shell_session.expect(PROMPT)
-
-    result = get_command_output(shell_session.before)
-    assert "Hello" == result
-
-
 def test_error_subshell_with_missing_closing_parenthesis(shell_session):
     shell_session.sendline("(echo Hello")
     shell_session.expect(PROMPT)
@@ -60,6 +36,45 @@ def test_error_nested_subshell_with_error(shell_session):
     assert "minishell: syntax error near unexpected token `('" in result
 
 
+def test_error_empty_subshell(shell_session):
+    shell_session.sendline("()")
+    shell_session.expect(PROMPT)
+    result = get_command_output(shell_session.before)
+    assert "minishell: syntax error near unexpected token `)'" in result
+
+
+def test_error_subshell_after_cmd(shell_session):
+    shell_session.sendline("echo Hello (ls)")
+    shell_session.expect(PROMPT)
+
+    result = get_command_output(shell_session.before)
+    assert "minishell: syntax error near unexpected token `('" in result
+
+
+def test_simple_subshell(shell_session):
+    shell_session.sendline("(echo Hello)")
+    shell_session.expect(PROMPT)
+
+    result = get_command_output(shell_session.before)
+    assert "Hello" == result
+
+
+def test_subshell_with_pipeline(shell_session):
+    shell_session.sendline("(echo Hello) | ((cat) | (cat -e))")
+    shell_session.expect(PROMPT)
+
+    result = get_command_output(shell_session.before)
+    assert "Hello$" == result
+
+
+def test_subshell_with_pipeline_simple(shell_session):
+    shell_session.sendline("(ls -l) | (echo Hello)")
+    shell_session.expect(PROMPT)
+
+    result = get_command_output(shell_session.before)
+    assert "Hello" == result
+
+
 def test_subshell_with_redirect_inside(shell_session):
     shell_session.sendline("(echo Hello > /dev/null) ")
     shell_session.expect(PROMPT)
@@ -74,24 +89,9 @@ def test_subshell_with_redirect_outside(shell_session):
     assert "" in result
 
 
-def test_error_empty_subshell(shell_session):
-    shell_session.sendline("()")
-    shell_session.expect(PROMPT)
-    result = get_command_output(shell_session.before)
-    assert "minishell: syntax error near unexpected token `)'" in result
-
-
 def test_subshell_outside_of_pipeline(shell_session):
     shell_session.sendline("(echo Hello | echo World | (cat -e))")
     shell_session.expect(PROMPT)
 
     result = get_command_output(shell_session.before)
     assert "World$" == result
-
-
-def test_error_subshell_after_cmd(shell_session):
-    shell_session.sendline("echo Hello (ls)")
-    shell_session.expect(PROMPT)
-
-    result = get_command_output(shell_session.before)
-    assert "minishell: syntax error near unexpected token `('" in result
