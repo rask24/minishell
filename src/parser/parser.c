0ef906264339351aacf26aa181ce6ce42f4f3ddb
rask24 <rask0x18@gmail.com>
2024-10-23 13:29:09 +0900

    Handle systax error

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
+/*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 19:11:50 by reasuke           #+#    #+#             */
-/*   Updated: 2024/10/22 22:48:26 by yliu             ###   ########.fr       */
+/*   Updated: 2024/10/23 13:19:01 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 #include "parser_internal.h"
 #include "token.h"
+#include "utils.h"
+
+static t_ast	*handle_syntax_error(t_ast *node, t_token_list **cur_token)
+{
+	const char	*token_value;
+
+	token_value = get_token_value(*cur_token);
+	if (token_value == NULL)
+		token_value = "EOF";
+	print_syntax_error(token_value);
+	return (destroy_ast(node));
+}
 
 t_ast	*parser(t_token_list *token_list)
 {
@@ -22,9 +34,6 @@ t_ast	*parser(t_token_list *token_list)
 	cur_token = &token_list;
 	node = parse_list(cur_token);
 	if (node == NULL || get_token_type(*cur_token) != TOKEN_EOF)
-	{
-		handle_syntax_error(node, get_token_value(*cur_token));
-		return (NULL);
-	}
+		return (handle_syntax_error(node, cur_token));
 	return (node);
 }
