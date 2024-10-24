4a91c65fd744d9d8f3e85a56a6bbf60f13b14b28
rask24 <rask0x18@gmail.com>
2024-10-24 14:56:13 +0900

    Refactor parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 19:11:50 by reasuke           #+#    #+#             */
-/*   Updated: 2024/10/23 17:34:08 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/10/24 14:55:11 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@
 #include "token.h"
 #include "utils.h"
 
-static t_ast	*handle_syntax_error(t_ast *node, t_token_list **cur_token,
-					t_ctx *ctx)
+static t_ast	*handle_syntax_error(t_ast *node, t_token_list **cur_token)
 {
 	const char	*token_value;
 
@@ -27,7 +26,6 @@ static t_ast	*handle_syntax_error(t_ast *node, t_token_list **cur_token,
 	if (token_value == NULL)
 		token_value = "EOF";
 	print_syntax_error(token_value);
-	ctx->exit_status = 2;
 	return (destroy_ast(node));
 }
 
@@ -39,7 +37,10 @@ t_ast	*parser(t_token_list *token_list, t_ctx *ctx)
 	cur_token = &token_list;
 	node = parse_list(cur_token);
 	if (node == NULL || get_token_type(*cur_token) != TOKEN_EOF)
-		return (handle_syntax_error(node, cur_token, ctx));
+	{
+		ctx->exit_status = 2;
+		return (handle_syntax_error(node, cur_token));
+	}
 	else if (node->type == AST_UNKNOWN)
 	{
 		ctx->exit_status = 128 + SIGINT;
