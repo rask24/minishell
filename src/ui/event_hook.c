b39097ffdf8401e73983aa6d135eafb2a6ada88a
Reiya Asuke <70057885+rask24@users.noreply.github.com>
2024-10-24 15:21:06 +0900

    141 fix heredoc signal handler (#152)
    * Refactor create_heredoc
    
    * Handle SIGINT on heredoc
    
    * Refactor open_heredoc
    
    * Fix SIGSEGV on set_redirect_heredoc_info
    
    * Fix SIGINT on heredoc
    
    * Fix destroy_and_return
    
    * Update abort_parse_syntax_error
    
    * Rename abort_parse_return
    
    * Introduce heredoc status
    
    * Handle systax error
    
    * Fix double free
    
    * Delete expect_token
    
    * Fix parse_status
    
    * Fix heredoc SIGINT
    
    * Refactor parse_simple_command
    
    * Fix event_hook
    
    * Fix exit_status in parser
    
    * Fix norm
    
    * Add errno.h to create_heredoc
    
    * Refactor write_heredoc
    
    * Fix parse_command
    
    * Rename parser_utils
    
    * Add tests for handle_parse_stauts
    
    * Fix unit tests
    
    * Refactor parser
    
    * Fix handle_heredoc_sigint_hook

--- src/ui/event_hook.c
+++ src/ui/event_hook.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 23:34:34 by reasuke           #+#    #+#             */
-/*   Updated: 2024/10/18 22:17:55 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/10/24 15:10:05 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 #include "libft.h"
 #include "readline/readline.h"
 #include "ui.h"
+
+int	handle_heredoc_sigint_hook(void)
+{
+	if (g_signum == SIGINT)
+	{
+		rl_replace_line("", 0);
+		rl_done = 1;
+	}
+	return (0);
+}
 
 int	handle_sigint_hook(void)
 {
