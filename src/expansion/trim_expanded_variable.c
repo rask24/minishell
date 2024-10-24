da0df9b720fb7e906767c10e4dd1f962cdd1e8c4
yliu <yliu@XLIJFELKNIX.local>
2024-10-24 15:33:54 +0900

    Refactor code

--- src/expansion/trim_expanded_variable.c
+++ src/expansion/trim_expanded_variable.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 22:59:05 by yliu              #+#    #+#             */
-/*   Updated: 2024/10/23 21:11:00 by yliu             ###   ########.fr       */
+/*   Updated: 2024/10/24 15:33:21 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,13 @@ static bool	is_identifier_char(char c)
 static char	*resolve_env(char *candidate_key, t_env_list *env)
 {
 	char	*value;
-	char	*result;
 
 	if (!is_identifier(candidate_key))
 		return (ft_xstrdup(""));
 	value = lookup_value(candidate_key, env);
 	if (!value)
 		return (ft_xstrdup(""));
-	result = ft_xstrdup(value);
-	return (result);
+	return (ft_xstrdup(value));
 }
 
 char	*trim_expanded_variable(t_string *expand_info, t_ctx *ctx)
