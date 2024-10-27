d3315959ce0508a6610a9f533402163f6bb1bc92
rask24 <rask0x18@gmail.com>
2024-10-27 10:06:46 +0900

    Refactor handle_heredoc

--- src/parser/handle_heredoc.c
+++ src/parser/handle_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:38:07 by reasuke           #+#    #+#             */
-/*   Updated: 2024/10/25 20:14:50 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/10/27 10:04:51 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,32 @@
 #include "ui.h"
 #include "utils.h"
 
-static t_parse_status	calc_heredoc_status(t_list *input_list)
+static size_t	calc_heredoc_size(t_list *input_list)
 {
-	if (input_list && input_list->content == NULL)
-		return (PARSE_ABORT);
-	else
-		return (PARSE_SUCCESS);
+	size_t	size;
+
+	size = 0;
+	while (input_list)
+	{
+		size += ft_strlen(input_list->content) + 1;
+		input_list = input_list->next;
+	}
+	return (size);
 }
 
 static void	set_redirect_heredoc_info(t_redirect_info *info, t_list *input_list,
-			size_t heredoc_size, bool should_expand)
+				bool should_expand)
 {
+	size_t	heredoc_size;
+
+	heredoc_size = calc_heredoc_size(input_list);
 	if (!(input_list && input_list->content == NULL))
 		info->heredoc_fd = create_heredoc(input_list, heredoc_size);
 	info->heredoc_size = heredoc_size;
 	info->should_expand = should_expand;
 }
 
-static void	handle_sigint(t_list **input_list)
-{
-	g_signum = 0;
-	ft_lstadd_front(input_list, ft_xlstnew(NULL));
-}
-
-static t_list	*read_heredoc_input(const char *delimiter, size_t *heredoc_size)
+static t_list	*read_heredoc_input(const char *delimiter)
 {
 	char	*line;
 	t_list	*input_list;
@@ -59,31 +61,37 @@ static t_list	*read_heredoc_input(const char *delimiter, size_t *heredoc_size)
 		}
 		if (g_signum == SIGINT && line[0] == '\0')
 		{
-			handle_sigint(&input_list);
+			g_signum = 0;
+			ft_lstadd_front(&input_list, ft_xlstnew(NULL));
 			break ;
 		}
 		if (ft_strcmp(line, delimiter) == 0)
 			break ;
-		*heredoc_size += ft_strlen(line) + 1;
 		ft_lstadd_back(&input_list, ft_xlstnew(line));
 	}
 	free(line);
 	return (input_list);
 }
 
+static t_parse_status	calc_heredoc_status(t_list *input_list)
+{
+	if (input_list && input_list->content == NULL)
+		return (PARSE_ABORT);
+	else
+		return (PARSE_SUCCESS);
+}
+
 t_parse_status	handle_heredoc(const char *delimiter, t_redirect_info *info)
 {
 	t_list			*input_list;
-	size_t			heredoc_size;
 	char			*expanded_delimiter;
 	t_parse_status	status;
 
 	expanded_delimiter = expand_quotes((char *)delimiter);
-	heredoc_size = 0;
 	rl_event_hook = handle_heredoc_sigint_hook;
-	input_list = read_heredoc_input(expanded_delimiter, &heredoc_size);
+	input_list = read_heredoc_input(expanded_delimiter);
 	rl_event_hook = handle_sigint_hook;
-	set_redirect_heredoc_info(info, input_list, heredoc_size,
+	set_redirect_heredoc_info(info, input_list,
 		ft_strcmp(expanded_delimiter, delimiter) == 0);
 	status = calc_heredoc_status(input_list);
 	free(expanded_delimiter);
