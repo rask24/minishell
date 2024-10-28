6f092149e4325d35cd9de62cba42001ac2988b1b
yliu <yliu@student.42tokyo.jp>
2024-10-28 11:03:31 +0900

    Rename constructor_lexer -> constructor_lexer_info (#202)
    Co-authored-by: yliu <yliu@XLIJFELKNIX.local>

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,13 +6,21 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 14:59:01 by yliu              #+#    #+#             */
-/*   Updated: 2024/10/09 11:39:21 by yliu             ###   ########.fr       */
+/*   Updated: 2024/10/27 17:36:38 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "lexer_internal.h"
 #include "token.h"
+
+static void	construct_lexer_info(const char *input, t_lexer *lexer)
+{
+	lexer->left = input;
+	lexer->right = input;
+	lexer->value = NULL;
+	lexer->type = -1;
+}
 
 t_token_list	*lexer(const char *input)
 {
@@ -21,7 +29,7 @@ t_token_list	*lexer(const char *input)
 	t_lexer			lexer;
 
 	token_list = NULL;
-	construct_lexer(input, &lexer);
+	construct_lexer_info(input, &lexer);
 	while (true)
 	{
 		one_token = get_next_token(&lexer);
