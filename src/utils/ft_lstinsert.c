559fe17f6f88278f2c23361ec8c2a9bed5a27a3b
yliu <yliu@XLIJFELKNIX.local>
2024-11-25 18:50:07 +0900

    Add NULL guards.Also add code when lst is empty

--- src/utils/ft_lstinsert.c
+++ src/utils/ft_lstinsert.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:39:12 by yliu              #+#    #+#             */
-/*   Updated: 2024/11/25 11:39:15 by yliu             ###   ########.fr       */
+/*   Updated: 2024/11/25 18:48:36 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	ft_lstinsert(t_list **lst, t_list *trg, t_list *insert)
 {
 	t_list	*trg_prev;
 
-	if (lst == NULL || *lst == NULL)
+	if (lst == NULL || insert == NULL)
 		return ;
+	if (*lst == NULL)
+	{
+		*lst = insert;
+		return ;
+	}
 	if (trg == NULL)
 	{
 		ft_lstlast(*lst)->next = insert;
